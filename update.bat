936750c57ef896c1b5cf60e1a68663ddda0c4fb5
224466414 <1073582541@qq.com>
2021-07-24 19:28:49 +0800

    update

---
+++ update.bat
@@ -0,0 +1,3 @@
+start update\update_book.bat
+TIMEOUT /T 30
+start update\update_git.bat
---
+++ update/update_git.bat
@@ -0,0 +1,7 @@
+cd _book
+git init
+git add .
+git commit -m "update"
+git remote rm origin
+git remote add origin git@github.com:224466414/Quantitative_Investment_Help.git
+git push -f origin master
