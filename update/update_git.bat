aa87d2205ba829208c5d21b697adbc09b1baf886
224466414 <1073582541@qq.com>
2021-07-24 19:32:39 +0800

    update

--- update/update_git.bat
+++ update/update_git.bat
@@ -3,5 +3,5 @@ git init
 git add .
 git commit -m "update"
 git remote rm origin
-git remote add origin git@github.com:224466414/Quantitative_Investment_Help.git
+git remote add origin git@github.com:224466414/Quantitative_Investment_factorTest.git
 git push -f origin master
